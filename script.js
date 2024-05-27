b578fe22c57b7144e5843fd3ea9f5f79111e8f75
naseemnaz12 <164153238+naseemnaz12@users.noreply.github.com>
2024-05-27 13:57:33 +0500

    my assignment

---
+++ script.js
@@ -0,0 +1,233 @@
+
+//#2
+// var greeting ="Welcome to js land" ;
+// alert("Welcome to JS  Land...");
+//#3
+// var message = [`Happy Coding
+//                 Prevent this page from creating additional dialogs`
+// ];
+//#4
+// alert(`Happy Coding!
+//      Prevent this page from creating additional dialogs`
+// );
+//#5
+//  var message = "Hello...I can run JS through my web browser's console";
+//  alert("Hello...I can run JS through my web browsers console");
+//assignment # 1 
+//# 1
+// var greetings = "Hello! Welcome to our proficient and qualitative organization";
+// alert("Hello! Welcome to our proficient and qualitative organization");
+
+//# 2
+// var message = "Error! Please enter a valid password.";
+// alert("Error! Please enter a valid password.");
+
+//# 3
+// var message = [`Welcome to JS Land...
+//                 Happy coding!`
+// ];
+
+// alert(`Welcome to JS Land...
+// Happy coding!`
+// );
+//# 4
+//(1)
+//(i) var username;
+
+//(ii) var myname;
+// myname ="Naseem Naz";
+
+// (iii)
+//(a)// 
+// var message;
+// message = "Hello World";
+// alert("Hello World");
+// (iv)
+// var students_data = "John doe";
+// alert("John doe");
+// var students_data = "15 years old";
+// alert("15 years old");
+// var students_data = "Certified Mobile Application Development";
+// alert("Certified Mobile Application Development");
+// # 5
+// var pizza = [`PIZZA
+//               PIZZ
+//               PIZ
+//               PI
+//               P`
+
+// ];
+// alert(`PIZZA
+// PIZZ
+// PIZ
+// PI
+// P`)
+//# 6
+// var email;
+// email = "My email address is rapparolla66@gmail.com";
+// alert("My email address is "+" rapparolla66@gmail.com");
+
+//# 7
+// var book;
+// book = "A smarter way to learn javascript";
+// alert(`I am trying to learn from the book A smarter 
+// way to learn javascript`);
+
+//# 8
+// var information = "Yeah! I can write HTML content through JavaScript";
+// console.log("Yeah! I can write HTML content through JavaScript");
+
+//# 9
+// var ending = ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")                   
+// console.log("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");        
+// chap 2 complt
+
+
+// chap # 3
+// q 1
+// var age; 
+// age ="27 years";
+// alert("My age is 27 year");
+// q 2
+// var uservisits;
+// uservisits ="14 times";
+// console.log("14 times visited");
+// alert("You have visited this site 14 times");
+// q 3
+// var birthyear;
+// birthyear ="1998";
+// console.log("My birth year is 1998");
+// q 4
+// var name ="John Doe";
+// console.log("John Doe");
+// var productTitle =" T-Shirts(medium size)";
+// console.log(" T-Shirts(medium size)");
+// var productQuantity = "5 T-Shirt,medium size";
+// console.log("5 T-Shirt,medium size");
+// console.log("“John Doe ordered 5 T-shirt(s) on XYZ Clothing store”");
+
+// chap 4
+//  q 1
+
+
+//  q 2
+// 5 illegal variables
+// var "Naseem";
+// var 5birds;
+// var 50089;
+// var var;
+// var ##
+
+//  5 legal variables
+// var naseem_Naz;
+// console.log("naseem_naz");
+// var $1000;
+// console.log("$1000");
+// var github12;
+// console.log("github12");
+// var smitAlyabad;
+// console.log("smitAlyabad");
+// var messageABC;
+// console.log("messageABC");
+
+//  q 3
+// var legal = "Rules for naming JS variables"
+// console.log("Rules for naming JS variables");
+
+// var  contain = "Variables names can only contain $, letter _. For example: $my_1st Variable ";
+// console.log("Variables names can only contain $, letter _. For example: $my_1st Variable ");
+
+// var rule = "Variable must begin with a $name,_ or name";
+// console.log("Variable must begin with a $name,_name or name");
+
+// var rule = "Variable names are case sensitive";
+// console.log("Variable names are case sensitive");
+
+// var rule = "Variable name should not be JS keyword";
+// console.log("Variable name should not be JS keyword");
+
+                                // END
+    //  chap 5
+    //  q 1
+    // num1 = 5;
+    // num2 = 3;
+    // let sum = num1 + num2;
+    //  console.log("The sum of" +" "+ num1 +" "+ "and" +" "+ num2+" "+ "is" +" "+ sum);
+    
+    // // q 2
+    
+    // num1 = 180;
+    // num2 = 20;
+    // let substraction = num1- num2;
+    // console.log("The substraction of" +" "+ num1 +" "+ "and" +" "+ num2+" "+ "is" +" "+ substraction);
+    
+    // num1 = 20;
+    // num2 = 5;
+    // let multiplication = num1 * num2;
+    // console.log("multiplication of" +" "+ num1 +" "+ "and" +" "+ num2 +" "+ "is" +" "+multiplication);
+    
+    // num1 = 8;
+    // num2 = 4;
+    // let division = num1 / num2;
+    // console.log("Division of" +" "+ num1+" "+ "and"+" "+num2+" "+"is"+" "+division);
+    
+    // num1 = 17;
+    // num2 = 3;
+    // let modulus = num1 % num2;
+    // console.log("modulus  of" +" "+num1+" "+"and"+" "+num2+" " +"is" +" "+modulus);
+
+    // q 3
+    //a
+    // let myVariable;     //declear
+    
+    // //b                //show the value after declearation
+    // console.log("Value after variable declearation is:"+ myVariable);
+    // //c           value of variable
+    // myVariable = 5;        
+    // //d                 //show the initial value
+    // console.log("Initial value:"+myVariable);
+    // //e
+    // myVariable++;          //increment
+    // //f                                    
+    // console.log("Value after increment is:"+myVariable);    //value after increment
+    // //g                     
+    // myVariable += 7;             //addition of 7 in variable
+    // //h            //value after addition
+    // console.log("Value after addition is:"+myVariable);
+
+    // //i
+    // myVariable--;     //decrement
+    // //j               //value after decrement
+    // console.log("Value after decrement is:"+myVariable);
+    // //k     //Show the remainder after dividing the variable’s value by 3.
+    // var reminder = myVariable % 3;
+    // //l         //displaying reminder
+    // console.log("The reminder is:"+reminder);
+
+    // q 4
+    //  oneTicket = 600  ;
+    //  forbuy = 5;
+    //  let multiplication = 600 * 5;
+    //  console.log("Total cost to buy"+" "+forbuy+ " tickets to a movie is:" + multiplication);
+    // q 5
+    // console.log("Table of 5");
+    
+
+
+    // q 6
+    
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    
